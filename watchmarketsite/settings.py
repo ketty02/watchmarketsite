7d40486e67c85bad56801295c5bc40284ad62c0a
Bercia Razvan-Catalin <61936195+ketty02@users.noreply.github.com>
2020-05-04 21:41:25 +0300

    Revert "Add users app to project."

--- watchmarketsite/settings.py
+++ watchmarketsite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
 ]
 
 MIDDLEWARE = [
@@ -124,5 +123,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-AUTH_USER_MODEL = 'users.MyUser'
