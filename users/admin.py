87295361b2bf5ef930c456b778ce0640ab0fd7de
Razvan-Catalin Bercia <bercia_catalin@yahoo.com>
2020-05-12 19:13:01 +0300

    Add users app to project.

--- users/admin.py
+++ users/admin.py
@@ -1,40 +1,42 @@
 from __future__ import unicode_literals
-from django.utils.translation import gettext,gettext_lazy as _
+from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import MyUser
-from django.contrib.auth.forms import UserChangeForm,UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
 
 class MyUserCreationForm(UserCreationForm):
     class Meta :
         model = MyUser
         exclude = []
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
 
 class MyUserChangeForm(UserChangeForm):
-    class Meta :
+    class Meta:
         model = MyUser
         exclude = []
+
 
 @admin.register(MyUser)
 class MyUserAdmin(UserAdmin):
     add_form = MyUserCreationForm
     form = MyUserChangeForm
     fieldsets = (
-        (None, {'fields':('email','password')}),
-        (_('Personal info'),{'fields': ('first name', 'last name')}),
-        (_('Permissions'),{
-            'fields': ('is_active','is_staff','is_superuser','groups','user_permissions'),
+        (None, {'fields' : ('email', 'password')}),
+        (_('Personal info'), {'fields' : ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields' : ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
-        (_('Important dates'),{'fields': ('last_login', 'date_joined')})
+        (_('Important dates'), {'fields' : ('last_login', 'date_joined')})
     )
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('email','password1','password2'),
+            'classes' : ('wide',),
+            'fields' : ('email', 'password1', 'password2'),
         }),
     )
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    search_fields = ('email','first_name', 'last_name' )
+    search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
-
-
--- users/views.py
+++ users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from __future__ import unicode_literals
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login, logout
+from users.forms import LoginForm
+from django.http import HttpResponseRedirect
+from users.admin import MyUserCreationForm
 
-# Create your views here.
+
+def handle_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {
+        'form' : form
+    })
+
+
+def handle_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login'))
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect(reverse('users:login'))
+    else :
+        form = MyUserCreationForm()
+
+    return render(request, 'users/register.html', {
+        'form' : form
+    })
