7e7274b92ec72fd82d0de4055f5f6ef6f3e84f82
Razvan-Catalin Bercia <bercia_catalin@yahoo.com>
2020-05-21 22:16:25 +0300

    Add email to project.

--- watchmarketsite/settings.py
+++ watchmarketsite/settings.py
@@ -131,3 +131,10 @@ LOGIN_URL = 'users:login'
 
 MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bercia_catalin@yahoo.com'
+EMAIL_HOST_PASSWORD = 'wjfkftstgvlagofc'
