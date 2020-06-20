8b1fe33879d61c65507cbc92c1e3efc871cebc5f
Razvan-Catalin Bercia <bercia_catalin@yahoo.com>
2020-06-20 08:57:23 +0300

    populate database with tables using commands and register groups of users.

--- market/models/type.py
+++ market/models/type.py
@@ -6,6 +6,7 @@ from market.models.watch import Watch
 class Type(models.Model):
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
     watch = models.ManyToManyField(Watch, through='WatchType', related_name='watches')
+    watch_picture = models.FileField( upload_to='watch_picture', null=True )
     name = models.CharField(unique=True, max_length=255)
     case_material = models.CharField(max_length=255)
     bracelet_material = models.CharField(max_length=255)
@@ -18,6 +19,7 @@ class Type(models.Model):
 class WatchType(models.Model):
     watch = models.ForeignKey(Watch, on_delete=models.CASCADE)
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    actual_picture = models.FileField(upload_to='actual_picture', null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, validators=[
         MinValueValidator(0)
     ],)
--- market/models/watch.py
+++ market/models/watch.py
@@ -4,6 +4,7 @@ from django.conf import settings
 class Watch(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='watches')
     name = models.CharField(max_length=255)
+    brand_picture = models.FileField(upload_to='brand', null=True)
 
 
     class Meta:
