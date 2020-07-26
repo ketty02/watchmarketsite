11f4fecb21453a2088f6d327e18b1de91cf45c78
Razvan-Catalin Bercia <bercia_catalin@yahoo.com>
2020-07-26 13:33:31 +0300

    stripe payments added

--- users/pipeline.py
+++ users/pipeline.py
@@ -21,27 +21,27 @@ def create_user(strategy, details, backend, user=None, *args, **kwargs):
 
 
 def profile_picture(backend, user, response, is_new, *args, **kwargs):
-    if is_new:
-        url = None
-        params = {}
+    #if is_new:
+    url = None
+    params = {}
 
-        if backend.name == 'google-oauth2':
-            url = response['picture']
+    if backend.name == 'google-oauth2':
+        url = response['picture']
 
-        if backend.name == 'facebook':
-            url = 'http://graph.facebook.com/{USER_ID}/picture'.format(
-                USER_ID=response['id'],
-            )
-            params['type'] = 'large'
+    if backend.name == 'facebook':
+        url = 'http://graph.facebook.com/{USER_ID}/picture'.format(
+            USER_ID=response['id'],
+        )
+        params['type'] = 'large'
 
-        if url:
-            try:
-                image_response = request('GET', url, params=params)
-                image_response.raise_for_status()
-                print(image_response.headers)
-            except HTTPError:
-                pass
-            else:
-                profile = user.profile
-                profile.avatar.save(f'{uuid4()}.jpg', ContentFile(image_response.content))
-                profile.save()
+    if url:
+        try:
+            image_response = request('GET', url, params=params)
+            image_response.raise_for_status()
+            print(image_response.headers)
+        except HTTPError:
+            pass
+        else:
+            profile = user.profile
+            profile.avatar.save(f'{uuid4()}.jpg', ContentFile(image_response.content))
+            profile.save()
