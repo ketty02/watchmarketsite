8362dca04b358eb69d29bd3ba7194c5df61de3a1
Razvan-Catalin Bercia <bercia_catalin@yahoo.com>
2020-07-27 22:11:54 +0300

    Prepare deployment

---
+++ watchmarketsite/settings/prod.py
@@ -0,0 +1,28 @@
+from watchmarketsite.settings.base import *
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+ADMINS = [('Bercia Catalin', 'bercia_catalin@yahoo.com')]
+
+SERVER_EMAIL = 'bercia_catalin@yahoo.com'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_logs.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
